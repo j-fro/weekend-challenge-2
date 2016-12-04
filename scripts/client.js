cc1229c6fdcceaa93ef874b33b90eabbdef2118b
Jacob Froman <jacob.h.froman@gmail.com>
2016-12-04 10:34:07 -0600

    kinda hard mode complete

--- scripts/client.js
+++ scripts/client.js
@@ -15,6 +15,7 @@ function enableButtons() {
     /* Wires up all buttons to their click handlers */
     $(document).on('click', '#nextButton', nextClicked);
     $(document).on('click', '#prevButton', prevClicked);
+    $(document).on('click', '.studentButton', studentClicked);
 }
 
 function getStudentInfo(url) {
@@ -32,9 +33,10 @@ function parseStudentInfo(data) {
     studentData.forEach(function(student) {
         studentsArray.push(student);
     });
-    console.log(studentsArray);
     // Immediately display the first student
     displayStudentInfo(studentsArray[currentIndex]);
+    // Add buttons for each student to the DOM
+    displayStudentButtons(studentsArray);
 }
 
 /* --- CLICK HANDLERS --- */
@@ -49,19 +51,36 @@ function prevClicked() {
     displayStudentInfo(studentsArray[currentIndex]);
 }
 
+function studentClicked() {
+    console.log($(this).data());
+    var index = $(this).data().index;
+    currentIndex = index;
+    displayStudentInfo(studentsArray[currentIndex]);
+}
+
 /* --- DISPLAY FUNCTIONS --- */
 
 function displayStudentInfo(student) {
     /* Displays a single student's information on the DOM and updates position
     indicator */
     var $container = $('#studentDisplay');
+    $container.fadeOut();
     var htmlString = '<h2>' + student.first_name + ' ' + student.last_name + '</h2>';
     htmlString += '<img class="portrait" src="' + student.picUrl + '" />';
     htmlString += '<p>' + student.info + '</p>';
     $container.html(htmlString);
+    $container.fadeIn();
 
     var $position = $('#positionIndicator');
     $position.html(currentIndex + 1 + '/' + studentsArray.length);
+}
+
+function displayStudentButtons(array) {
+    /* Displays a button for each student between the prev and next buttons */
+    array.forEach(function(student, index) {
+        var $container = $('#studentButtons');
+        $container.append('<button class="studentButton" data-index="' + index + '"><img src=' + student.picUrl + ' /></button>');
+    });
 }
 
 /* --- UTILITY FUNCTIONS --- */
